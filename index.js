65ddc457bb620e87872453b7c81d894ed0948969
ATALLAH123 <2atallahouhou@gmail.com>
2023-06-26 18:21:36 +0100

    first

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ import cors from 'cors'
 const port = process.env.PORT || 8000;
 const app = express();
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 dotenv.config();
